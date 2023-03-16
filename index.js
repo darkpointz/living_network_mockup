3b9fb81ab5eeb4698ee003470a36cc27c2ad56e7
pipap443 <pipap443@entro.postbox.in.th>
2023-03-16 11:15:36 +0700

    ddd

--- index.js
+++ index.js
@@ -93,11 +93,9 @@ app.post("/save_userdata", jsonParser, async (req, res) => {
 
 app.post("/remove_userdata", jsonParser, async (req, res) => {
   try {
-    await living_network.remove([
-      {
-        _id: req.body.token,
-      },
-    ]);
+    await living_network.remove({
+      _id: req.body.token,
+    });
     res.send("Remove UserData Success");
   } catch (error) {
     console.log("err : " + error);
