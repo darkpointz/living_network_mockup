1eaf649d77d6861f7e20a2ad8c09c4ea43a94cc2
pipap443 <pipap443@entro.postbox.in.th>
2023-03-16 14:36:23 +0700

    adasd

--- models/living_network.js
+++ models/living_network.js
@@ -1,22 +1,24 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
-const LNSchema = new Schema({
-  title: {
-    type: String,
-    required: false,
+const LNSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: false,
+    },
+    body: {
+      type: String,
+      required: false,
+    },
+    token: {
+      type: String,
+      required: false,
+    },
+    UserData: Object,
+    _id: String,
   },
-  body: {
-    type: String,
-    required: false,
-  },
-  token : {
-    type: String,
-    required: false,
-  },
-  UserData: Object,
-  _id: String,
-  versionKey: false
-});
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("LN", LNSchema);
