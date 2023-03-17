55ac3773a0614fd32e26779c0b9978a5f813e883
pipap443 <pipap443@entro.postbox.in.th>
2023-03-17 13:37:59 +0700

    add location wifi

--- routes.js
+++ routes.js
@@ -4,10 +4,10 @@ const performance = require("./models/performance");
 const location = require("./models/location.js");
 
 const location_mockup = require("./models/json/location_mockup.json");
-// const location_mockup2 = require("./models/json/location_mockup2.json");
+const location_mockup2 = require("./models/json/locations_wifi.json");
 
 module.exports = (app) => {
-  app.get("/create_locations", async (req, res) => {
+  app.get("/create_locations_AISSHOP", async (req, res) => {
     try {
       const locationD = await location.create({
         _id: "LM_CCSM_AISSHOP_BKK",
@@ -21,23 +21,32 @@ module.exports = (app) => {
     }
   });
 
-  // app.get("/create_locations2", async (req, res) => {
-  //   try {
-  //     const locationD = await location.create({
-  //       _id: "LM_WIFI_AP_BKK",
-  //       type: location_mockup2.type,
-  //       crs: location_mockup2.crs,
-  //       features: location_mockup2.features,
-  //     });
-  //     res.send("Add location Success \n" + locationD);
-  //   } catch (error) {
-  //     console.log("err : " + error);
-  //   }
-  // });
+  app.get("/create_locations_wifi", async (req, res) => {
+    try {
+      const locationD = await location.create({
+        _id: "LM_WIFI_AP_BKK",
+        type: location_mockup2.type,
+        crs: location_mockup2.crs,
+        features: location_mockup2.features,
+      });
+      res.send("Add location Success \n" + locationD);
+    } catch (error) {
+      console.log("err : " + error);
+    }
+  });
 
-  app.get("/get_locations", async (req, res) => {
+  app.get("/get_locations_AISSHOP", async (req, res) => {
     try {
       const locationD = await location.findOne({ _id: "LM_CCSM_AISSHOP_BKK" });
+      locationD != null ? res.json(locationD) : res.send("Something went wrong");
+    } catch (error) {
+      console.log("err : " + error);
+    }
+  });
+
+  app.get("/get_locations_AISWIFI", async (req, res) => {
+    try {
+      const locationD = await location.findOne({ _id: "LM_WIFI_AP_BKK" });
       locationD != null ? res.json(locationD) : res.send("Something went wrong");
     } catch (error) {
       console.log("err : " + error);
